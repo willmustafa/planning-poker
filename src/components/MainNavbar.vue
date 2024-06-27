de719a2de557f2d26dc8af518d516f04bd7246c2
willian mustafa <willian@integra.do>
2024-06-27 19:51:38 -0300

    logout

--- src/components/MainNavbar.vue
+++ src/components/MainNavbar.vue
@@ -1,4 +1,15 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useRoute, useRouter } from 'vue-router'
+import { useUserStore } from '@/stores/user.store'
+
+const router = useRouter()
+const route = useRoute()
+const userStore = useUserStore()
+function logout() {
+  router.push('/')
+  userStore.user = null
+}
+</script>
 
 <template>
   <nav class="navbar navbar-expand-lg py-4 fixed-top">
@@ -15,10 +26,10 @@
       >
         <span class="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <div class="collapse navbar-collapse" v-if="route.path === ''">
         <ul class="navbar-nav mb-2 mb-lg-0 ms-auto">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Sair</a>
+            <a class="nav-link active" aria-current="page" href="#" @click="logout">Sair</a>
           </li>
         </ul>
       </div>
